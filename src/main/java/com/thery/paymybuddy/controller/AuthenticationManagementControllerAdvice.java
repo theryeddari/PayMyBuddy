fac3d858db608d0426f95d198c42a8d6e9f4c4d1
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-07 17:40:00 +0200

    add bad credential of spring security about token jwt into authentication controller advice for redirection in frontend.

--- src/main/java/com/thery/paymybuddy/controller/AuthenticationManagementControllerAdvice.java
+++ src/main/java/com/thery/paymybuddy/controller/AuthenticationManagementControllerAdvice.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -45,6 +46,10 @@ public class AuthenticationManagementControllerAdvice {
         logger.error("{}", ex.getMessage());
         if (ex.getCause() instanceof FindByEmailException) {
             return new ResponseEntity<>(CLIENT_NOT_FOUND_EXCEPTION, HttpStatus.NOT_FOUND);
+        }
+        if(ex.getCause() instanceof BadCredentialsException) {
+            logger.error("{}", ex.getCause().getMessage());
+            return new ResponseEntity<>(ex.getCause().getMessage(),HttpStatus.UNAUTHORIZED);
         }
         return new ResponseEntity<>(SIGN_IN_CLIENT_EXCEPTION, HttpStatus.INTERNAL_SERVER_ERROR);
     }
